623870960275c752014bd18fa4c1e98d5ecbfdb1
João Vitor dos Santos <jsantos@isolutions.com.br>
2022-04-28 15:27:28 -0300

    Adicionar arquivos de projeto.

---
+++ Escopo/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Estudo de escopo em estruturas condicionais");
+
+int idade = 18;
+bool acompanhado = true;
+string mensagemAdicional; // Sem essa declaração há erro, pois variáveis declaradas dentro de if são locais, e não podem ser acessadas fora do if.
+
+if (acompanhado == true)
+    mensagemAdicional = "Está acompanhado";
+else
+    mensagemAdicional = "Não está acompanhado";
+
+if (idade >= 18 || acompanhado == true)
+{
+    Console.WriteLine("Pode entrar.");
+    Console.WriteLine(mensagemAdicional);
+}
+else
+    Console.WriteLine("Não pode entrar");
+
+Console.ReadLine();
